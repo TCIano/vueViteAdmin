ccafafc8d594d7bdfac24190b86ee796f41d3161
TCIano <740409310@qq.com>
2023-03-30 13:47:52 +0800

    build: 生成环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,53 @@
-import { defineConfig } from 'vite'
+import { defineConfig, } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Components from 'unplugin-vue-components/vite'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  server: {
-    open: true
-  },
-  plugins: [
-    vue(),
-    Components(),
-    vueSetupExtend()
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, 'src')
-        //为 /src配置别名@
-      }
-    ]
-  }
-})
+export default ({ mode }) => {
+  const dev = mode === 'development'
+  console.log(dev);
+
+  return defineConfig({
+    base: dev ? '/' : '/vueViteAdmin/',
+    server: {
+      open: true
+    },
+    plugins: [
+      vue(),
+      Components(),
+      vueSetupExtend()
+    ],
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, 'src')
+          //为 /src配置别名@
+        }
+      ]
+    }
+  })
+}
+// export default defineConfig({
+
+
+//   base: '/',
+//   server: {
+//     open: true
+//   },
+//   plugins: [
+//     vue(),
+//     Components(),
+//     vueSetupExtend()
+//   ],
+//   resolve: {
+//     alias: [
+//       {
+//         find: "@",
+//         replacement: path.resolve(__dirname, 'src')
+//         //为 /src配置别名@
+//       }
+//     ]
+//   }
+// })
